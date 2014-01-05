8a62c21950db3fbffe66d94792e5a708b3aceacc
Emerson H V <ehvasquez@hotmail.com>
2014-01-04 23:40:23 -0500

    c26

--- js/index.js
+++ js/index.js
@@ -48,7 +48,8 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        //console.log('Received Event: ' + id);
+        alert('Received Event: ' + id);
     },
 
     scan: function() {
@@ -75,7 +76,7 @@ var app = {
             }
             */
         }, function (error) { 
-            console.log("Scanning failed: ", error); 
+            alert("Scanning failed: ", error); 
         } );
     },
 
